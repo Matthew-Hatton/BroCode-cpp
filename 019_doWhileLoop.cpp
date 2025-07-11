44e82a84990647ae479c767b391df6ce8c9e5a32
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-11 14:41:51 +0100

    dowhile code

--- 019_doWhileLoop.cpp
+++ 019_doWhileLoop.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+// do while loop = do some block of code first,
+//                  THEN repeat again if condition is true
+
 int main(){
-    
+
+    int number;
+    // This is all very cumbersome
+    /*
+    std::cout << "Enter a +ve: ";
+    std::cin >> number;
+    // we haven't assigned the number yet, so it won't work
+    while (number < 0 )
+    {
+        std::cout << "Enter a +ve: ";
+        std::cin >> number;
+    }
+    std::cout << "The # is: " << number;
+    */
+    // better to 'do while'
+    do{
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+    }while(number < 0);
     return 0;
 }
