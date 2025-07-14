4b30b96d9a2f5fbd103af6a618108cdd8b922dd4
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-14 11:40:01 +0100

    variable scope

---
+++ 029_variableScope.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// local variables = declared inside a function or block {}
+// global variables = declared outside of all functions
+
+int myNum = 3; // global variable - best to avoid
+
+void printNum();
+
+int main(){
+    int myNum = 1; // functions use local BEFORE global
+    printNum();
+    std::cout << myNum << "\n";
+    //if you would prefer to use global, you can pre-seed
+    std::cout << ::myNum; //uses global version
+    return 0;
+}
+
+void printNum(){
+    int myNum = 2; // two versions of same variable are fine within the same scope
+    std::cout << myNum << "\n"; // functions can't see inside other functions
+}
