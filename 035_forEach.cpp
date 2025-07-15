e6995019388ef11bb6d219235dd81515d865cc1d
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-15 14:38:00 +0100

    foreach loop

--- 035_forEach.cpp
+++ 035_forEach.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 // foreach loop = loop that eases the traversal
 //                  over an iterable data set
+//                  less flexibility. Starts at starts and goes to end
+//                  can't skip elements
 
 int main(){
-
+    std::string students[] = {"Matty","Jasmine","Jemima"};
+    for(std::string i : students){ // for each
+        std::cout << i << '\n';
+    }
+    
+    int grades[] = {65,72,44,26};
+    for(int grade : grades){
+        std::cout << grade << '\n';
+    }
     return 0;
 }
