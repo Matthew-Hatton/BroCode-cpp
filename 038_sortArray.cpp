b001d20c24916cb7ed7166b85ef8bafc0df70439
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-17 13:41:03 +0100

    array sorting

---
+++ 038_sortArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+// bubble sort
+// start at element 1, if element on right is larger, move element 1 to temp, move element 2 to element 1, then move temp into 2, then repeat
+void sort(int array[], int size);
+
+int main(){
+    int array[] = {10,2,6,5,3,9};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array,size);
+    for (int element : array)
+    {
+        std::cout << element << " ";
+    }
+    
+    return 0;
+}
+
+void sort(int array[], int size){
+    int temp;
+    for(int i = 0; i < size -1; i++){//-1 due to we don't need to compare last element against anything
+        for(int j=0; j < size - i -1; j++){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j + 1] = temp;        
+            }
+        }
+    
+    }
+}
