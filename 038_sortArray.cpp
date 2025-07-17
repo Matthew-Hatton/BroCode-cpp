08431577e1b26c35da5987ac8cb94536c3875c19
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-17 13:46:02 +0100

    comment

--- 038_sortArray.cpp
+++ 038_sortArray.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 // bubble sort
-// start at element 1, if element on right is larger, move element 1 to temp, move element 2 to element 1, then move temp into 2, then repeat
+// start at element 1, if element on right is larger, 
+//move element 1 to temp, move element 2 to element 1,
+// then move temp into 2, then repeat
+
 void sort(int array[], int size);
 
 int main(){
-    int array[] = {10,2,6,5,3,9};
-    int size = sizeof(array)/sizeof(array[0]);
+    int array[] = {10,2,6,5,3,9}; // declare array
+    int size = sizeof(array)/sizeof(array[0]); // find size
 
     sort(array,size);
+
     for (int element : array)
     {
         std::cout << element << " ";
-    }
+    } // print
     
     return 0;
 }
 
 void sort(int array[], int size){
-    int temp;
-    for(int i = 0; i < size -1; i++){//-1 due to we don't need to compare last element against anything
-        for(int j=0; j < size - i -1; j++){
-            if(array[j] > array[j + 1]){
+    int temp; // declare temp
+    // will need to iterate of array (size) times
+    for(int i = 0; i < size - 1; i++){ //-1 due to we don't need to compare last element against anything
+        for(int j = 0; j < size - i -1; j++){
+            if(array[j] > array[j + 1]){ // descending would be <
                 temp = array[j];
                 array[j] = array[j+1];
-                array[j + 1] = temp;        
+                array[j + 1] = temp; // swap the elements
             }
         }
     
