57206d8f9697d21f277088874030ac861fe437b7
Matthew-Hatton <matthew.hatton@strath.ac.uk>
2025-07-21 12:47:54 +0100

    pointers

--- 047_pointers.cpp
+++ 047_pointers.cpp
@@ -16,10 +16,24 @@ int main(){
     std::string freePizzas[5] = {"pizza1","pizza2","pizza3","pizza4","pizza5"};
     std::string *pFreePizzas = freePizzas; // arrays are already an address, so we don't need to use &
 
-    std::cout << pName << '\n'; // contains address of variable 8 bytes
-    std::cout << *pName << '\n'; // contains value at that address 32 bytes
+    std::cout << "1. " << pName << '\n'; // contains address of variable 8 bytes
+    std::cout << "2. " << *pName << '\n'; // contains value at that address 32 bytes
 
-    std::cout << pAge << '\n';
-    std::cout << *pAge << '\n';
+    std::cout << "3. " << pAge << '\n';
+    std::cout << "4. " << *pAge << '\n';
+
+    std::cout << "5. " << pFreePizzas << '\n';
+    std::cout << "6. " << *pFreePizzas << '\n';
+
+    for(std::string pizza : freePizzas)
+    {
+        std::cout << pizza << '\n';
+    }
+
+    for (int i = 0; i < sizeof(freePizzas)/sizeof(freePizzas[0]); i++)
+    {
+        std::cout << freePizzas[i] << '\n';
+    }
+    
     return 0;
 }
